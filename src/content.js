090e0aa7ca3c3d28b0febc22ee07fb64df3ed215
oliveirabruno01m <brunocabeludo321@gmail.com>
2023-05-20 23:30:18 -0300

    Add extension source code

---
+++ src/content.js
@@ -0,0 +1,19 @@
+// content.js
+const continueButtonSelector = "form > div > div:nth-child(1) > div > button:nth-child(2)"
+
+// Function to find and click the "continue" button
+function clickContinueButton() {
+    const continueButton = document.querySelector(continueButtonSelector);
+    if (continueButton) {
+        continueButton.click();
+    }
+}
+
+// Function to continuously check for the "continue" button
+function checkForContinueButton() {
+    clickContinueButton();
+    setTimeout(checkForContinueButton, 1000); // Check every 1 second
+}
+
+// Start checking for the "continue" button when the page finishes loading
+window.addEventListener("load", checkForContinueButton);
